dcafe6661bc1171bce6e135f3f017da39cac93d4
C5m7b4 <c5m7b4@gmail.com>
2024-12-10 13:27:16 -0600

    adds styles

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,15 @@ function App() {
   return (
     <div className="h-screen w-screen bg-slate-500 text-white flex flex-col justify-center items-center">
       <div>Hello</div>
-      <DynaSelect
-        data={data}
-        displayKey={"name"}
-        valueKey={"index"}
-        label={"Select a Person"}
-        onSelect={(e) => console.log(e)}
-      />
+      <div className="w-full px-8">
+        <DynaSelect
+          data={data}
+          displayKey={"name"}
+          valueKey={"index"}
+          label={"Select a Person"}
+          onSelect={(e) => console.log(e)}
+        />
+      </div>
     </div>
   );
 }
