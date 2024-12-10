be4b6780a7e31e3c1732672f7229cb24be635d2c
C5m7b4 <c5m7b4@gmail.com>
2024-12-10 12:55:08 -0600

    adds data setup

---
+++ src/data/index.ts
@@ -0,0 +1,71 @@
+interface iData {
+  index: number;
+  name: string;
+}
+
+export const data: iData[] = [
+  {
+    index: 0,
+    name: "Gilliam Barrera",
+  },
+  {
+    index: 1,
+    name: "Buckner Powell",
+  },
+  {
+    index: 2,
+    name: "Kirkland Shelton",
+  },
+  {
+    index: 3,
+    name: "Tanisha Mckenzie",
+  },
+  {
+    index: 4,
+    name: "Rose Brooks",
+  },
+  {
+    index: 5,
+    name: "Sonya William",
+  },
+  {
+    index: 6,
+    name: "Castaneda Livingston",
+  },
+  {
+    index: 7,
+    name: "Fry George",
+  },
+  {
+    index: 8,
+    name: "White Barnett",
+  },
+  {
+    index: 9,
+    name: "Hudson Nielsen",
+  },
+  {
+    index: 10,
+    name: "Isabella Knox",
+  },
+  {
+    index: 11,
+    name: "Aguirre Ellis",
+  },
+  {
+    index: 12,
+    name: "Alicia Brady",
+  },
+  {
+    index: 13,
+    name: "Lela Ware",
+  },
+  {
+    index: 14,
+    name: "Floyd Walsh",
+  },
+  {
+    index: 15,
+    name: "Carey Hunter",
+  },
+];
